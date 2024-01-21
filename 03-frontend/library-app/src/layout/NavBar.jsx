785cf4c2bf1e50dcd5dc744f9006eb39948ff1c1
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-01-20 23:46:03 -0500

    added logo color and styles

--- 03-frontend/library-app/src/layout/NavBar.jsx
+++ 03-frontend/library-app/src/layout/NavBar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
           <a href="/" className="mr-4 p-2">
             Link 3
           </a>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          <button className="bg-[#8A42F0] hover:bg-[#AB22FE] text-white font-bold py-2 px-4 rounded-full">
             Sign Up
           </button>
         </div>
