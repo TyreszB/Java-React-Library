3d1a1e3bcb5286eec0bba73cf1489df57108ad2b
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-01-20 23:22:51 -0500

    created navbar

---
+++ 03-frontend/library-app/src/App.js
@@ -0,0 +1,9 @@
+import NavBar from "./layout/NavBar";
+
+export default function App() {
+  return (
+    <main className="bg-[#DEE7E7]">
+      <NavBar />
+    </main>
+  );
+}
---
+++ 03-frontend/library-app/src/layout/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Logo from "../images/library-logo.png";
+
+const NavBar = () => {
+  return (
+    <nav className="bg-[#B7ADCF]">
+      <ul className="flex justify-between">
+        <div className="p-3 ">
+          <img
+            src={Logo}
+            alt="Library Logo"
+            className="rounded-full w-[75px] h-[75px]"
+          />
+        </div>
+        <div className="flex justify-around items-center p-3">
+          <a href="/" className="mr-4 p-2">
+            Link 1
+          </a>
+          <a href="/" className="mr-4 p-2">
+            Link 2
+          </a>
+          <a href="/" className="mr-4 p-2">
+            Link 3
+          </a>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Sign Up
+          </button>
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
